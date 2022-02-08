418e150f821cf8d5f50e37c4051deddb94e67b07
JBanks00 <twitchedjoe@gmail.com>
2022-02-08 09:42:15 +0000

    neated

--- test.js
+++ test.js
@@ -7,20 +7,20 @@ var url = 'http://192.168.56.10:2375';
 
 //this uses the simple get request from request
 //
-function containerQty(){
- request.get({
- //we are using the /info url to get the base docker information
- url: url + "/info",
- }, (err, res, data) => {
- if (err) {
- console.log('Error:', err);
- } else if (res.statusCode !== 200) {
- console.log('Status:', res.statusCode);
- } else{
- //we need to parse the json response to access
- data = JSON.parse(data)
- console.log("Number of Containers = " + data.Containers);
- }
- });
+function containerQty() {
+    request.get({
+        //we are using the /info url to get the base docker information
+        url: url + "/info",
+    }, (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+        } else {
+            //we need to parse the json response to access
+            data = JSON.parse(data)
+            console.log("Number of Containers = " + data.Containers);
+        }
+    });
 }
 containerQty();
